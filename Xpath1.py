442c28d7dbe586f6ad0dffde4d065fdbbd6654ab
Герман <116346468+german-skilyagin@users.noreply.github.com>
2023-11-16 15:44:18 +0500

    Delete Xpath1.py

--- Xpath1.py
+++
@@ -1,9 +0,0 @@
-import xml.etree.ElementTree as ET
-tree = ET.parse('movies.xml')
-root = tree.getroot()
-movies = {}
-for movie in root.findall('Movie'):
-    title = movie.find('Title').text
-    runtime = movie.get('runtime', 1)
-    movies[title] = runtime
-print (movies)
